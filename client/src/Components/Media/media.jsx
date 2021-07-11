e191c3bb3e18fc378aec215f9278257beb849d65
404th <umarov.doniyor.2002@gmail.com>
2021-07-11 22:55:54 +0500

    next comp is ready

---
+++ client/src/Components/Media/media.jsx
@@ -0,0 +1,46 @@
+import "./style/scss_media.scss";
+import ReactPlayer from "react-player/youtube";
+
+function Media() {
+    return (
+        <div className="media_wrapper">
+            <div className="media_container">
+                <div className="media_container_content">
+                    <p className={"media_container_content_first-p"}>
+                        Приступим cегодня
+                    </p>
+                    <p className={"media_container_content_second-p"}>
+                        Напишите нам, чтобы получить персональное предложение
+                    </p>
+                </div>
+                <div className="media_container_media">
+                    <div className="media_container_media_title">
+                        <p className="media_container_media_title_first-p">
+                            Cтроительство дома, не покажешь картинкой
+                        </p>
+                        <p className="media_container_media_title_second-p">
+                            За каждым домом стоит целая история, смотри полезные
+                            презентации у нас на канале.{" "}
+                        </p>
+                    </div>
+                    <div className="media_container_media_button">
+                        <button className="media_container_media_button_btn">
+                            Заказать расчет
+                        </button>
+                    </div>
+                    <div className="media_container_media_video">
+                        <ReactPlayer
+                            className={"media_container_media_video_ifr"}
+                            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+                            playing={true}
+                            width={  window.innerWidth > 856 ? "100%" : "320px" }
+                            height={ window.innerWidth > 856 ? "100%" : "274px" }
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Media;
--- client/src/Container/Main/main.jsx
+++ client/src/Container/Main/main.jsx
@@ -2,12 +2,14 @@ import "./style/scss_main.scss";
 
 import Banner from "../../Components/Banner/banner";
 import Usluga from "../../Components/Usluga/usluga";
+import Media from "../../Components/Media/media";
 
 function Main() {
     return (
         <>
             <Banner />
             <Usluga />
+            <Media />
         </>
     );
 }
