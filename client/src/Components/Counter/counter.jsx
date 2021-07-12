bab4068ea0b75a6d2c6a15be0fc61c86be41df6a
404th <umarov.doniyor.2002@gmail.com>
2021-07-12 23:10:54 +0500

    Manager part is ready

--- client/src/Components/Counter/counter.jsx
+++ client/src/Components/Counter/counter.jsx
@@ -1,7 +1,42 @@
 import "./style/scss_counter.scss";
 
 function Counter() {
-    return <div className={"counter_wrapper"}></div>;
+    return (
+        <div className={"counter_wrapper"}>
+            <div className="counter_container">
+                <div className="counter_container_div">
+                    <h3 className={"counter_container_div_h3"}>25</h3>
+                    <p className={"counter_container_div_p"}>
+                        построенных домов
+                    </p>
+                </div>
+                <div className="counter_container_div">
+                    <h3 className={"counter_container_div_h3"}>15</h3>
+                    <p className={"counter_container_div_p"}>
+                        построенных домов
+                    </p>
+                </div>
+                <div className="counter_container_div">
+                    <h3 className={"counter_container_div_h3"}>41</h3>
+                    <p className={"counter_container_div_p"}>
+                        построенных домов
+                    </p>
+                </div>
+                <div className="counter_container_div counter_container_div_youtube">
+                    <img
+                        className={"counter_container_div_youtube_img"}
+                        src="./images/counter/ytube.svg"
+                        alt="youtubeni rasmi"
+                        height={"60px"}
+                    />
+                    <p className={"counter_container_div_p"}>
+                        Наши ВИДЕО об отделке и устройстве смотрите на канале
+                        DOM-D
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Counter;
